067a71e3a4521c08978778080bf12763206cb68d
Kiki1875b <kevinheo0413@gmail.com>
2024-09-20 16:12:32 +0900

    FIX: Sequelize -> Prisma 이동

--- dist/app.js
+++ dist/app.js
@@ -4,28 +4,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const sequelize_1 = require("./config/sequelize");
+const prisma_1 = __importDefault(require("./prisma"));
 const app = (0, express_1.default)();
-const PORT = 3000;
+// 미들웨어 설정
 app.use(express_1.default.json());
+const PORT = process.env.PORT || 3000;
 app.get('/', async (req, res) => {
     try {
-        await sequelize_1.sequelize.authenticate(); // DB 연결 테스트
-        console.log('DB connection has been established successfully.');
-        sequelize_1.sequelize.sync({ alter: true })
-            .then(() => {
-            console.log("모델과 데이터베이스가 일치합니다.");
-        })
-            .catch(error => {
-            console.error("모델과 데이터베이스 간 일치하지 않습니다:", error);
-        });
-        res.status(200).send('DB connection is successful!');
+        console.log("접속 성공");
     }
     catch (error) {
-        console.error('Unable to connect to the DB:', error);
-        res.status(500).send('Error connecting to database');
+        res.status(500).json({ error: "에러 발생" });
+    }
+});
+app.get('/test', async (req, res) => {
+    try {
+        // DB 연결 확인 (간단한 쿼리 실행)
+        await prisma_1.default.$queryRaw `SELECT 1`; // 테스트 쿼리
+        res.status(200).json({ message: "DB 연결 성공" });
+    }
+    catch (error) {
+        console.error("DB 연결 실패:", error);
+        res.status(500).json({ error: "DB 연결 실패" });
     }
 });
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+});
+process.on('SIGINT', async () => {
+    await prisma_1.default.$disconnect();
+    process.exit(0);
 });
